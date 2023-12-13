5cda3a27ff9ed01456e0a677d749a86c35ca0270
Normunds Lazdins <normundslazdins16@gmail.com>
2023-12-13 23:50:07 +0200

    final touches

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {GoogleMap, LoadScript, Marker, Polyline} from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
 import axios from 'axios';
 
+// Map styling
 const mapContainerStyle = {
   width: '100%',
   height: '300px',
 };
 
+// Mapon API details
 const maponApiBaseUrl = 'https://mapon.com/api/v1';
 const maponApiKey = process.env.REACT_APP_MAPON_KEY;
 
 function App() {
+  // State variables
   const [selectedVehicle, setSelectedVehicle] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
@@ -22,6 +25,7 @@ function App() {
   const [totalDistance, setTotalDistance] = useState(0);
   const [totalTime, setTotalTime] = useState({ hours: 0, minutes: 0 });
 
+  // Fetch vehicle data
   useEffect(() => {
     const fetchVehicleData = async () => {
       try {
@@ -35,6 +39,7 @@ function App() {
     fetchVehicleData();
   }, []);
 
+  // Generate route based on user input
   async function generateRoute() {
     const formattedStartDateISO = startDate ? new Date(startDate) : null;
     const formattedEndDateISO = endDate ? new Date(endDate) : null;
@@ -54,138 +59,147 @@ function App() {
       setRouteCoordinates(filteredRoutes);
 
       let totalDistance = 0;
-      let totalTime = 0;
-  
+      let totalTimeInSeconds = 0;
+
       filteredRoutes.forEach(route => {
         totalDistance += route.distance;
-  
+
         const startTime = new Date(route.start.time);
         const endTime = new Date(route.end.time);
         const durationInSeconds = (endTime - startTime) / 1000; // Convert milliseconds to seconds
-        totalTime += durationInSeconds;
+        totalTimeInSeconds += durationInSeconds;
       });
-  
+
       // Convert totalDistance from meters to kilometers
       const totalDistanceInKm = totalDistance / 1000;
-  
-      // Convert totalTime to hours and minutes
-      const totalHours = Math.floor(totalTime / 3600);
-      const totalMinutes = Math.floor((totalTime % 3600) / 60);
-  
+
+      // Convert totalTimeInSeconds to hours and minutes
+      const totalHours = Math.floor(totalTimeInSeconds / 3600);
+      const totalMinutes = Math.floor((totalTimeInSeconds % 3600) / 60);
+
       setTotalDistance(totalDistanceInKm);
       setTotalTime({ hours: totalHours, minutes: totalMinutes });
-  
+
     } catch (error) {
       console.error('Error fetching route data from Mapon API:', error);
     }
   }
 
-
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
 
-        <div className="container">
-          <div className="container-report">
-            <h1>Route Report</h1>
-            <form>
-              <div className="vehicle">
-                <label htmlFor="vehicleNumber">
-                  Vehicle Number<span className="required">*</span>
-                </label>
-                <div className="vehi-margin">
-                  <select
-                      id="vehicleNumber"
-                      name="vehicleNumber"
-                      value={selectedVehicle}
-                      onChange={(e) => setSelectedVehicle(e.target.value)}
-                  >
-                    <option value="">Select vehicle</option>
-                    {vehicleList.map((vehicle) => (
-                        <option key={vehicle.number} value={vehicle.unit_id}>
-                          {vehicle.number}
-                        </option>
-                    ))}
-                  </select>
+      <div className="container">
+        <div className="container-report">
+          <h1>Route Report</h1>
+          <form>
+            {/* Vehicle selection */}
+            <div className="vehicle">
+              <label htmlFor="vehicleNumber">
+                Vehicle Number<span className="required">*</span>
+              </label>
+              <div className="vehi-margin">
+                <select
+                  id="vehicleNumber"
+                  name="vehicleNumber"
+                  value={selectedVehicle}
+                  onChange={(e) => setSelectedVehicle(e.target.value)}
+                >
+                  <option value="">Select vehicle</option>
+                  {vehicleList.map((vehicle) => (
+                    <option key={vehicle.number} value={vehicle.unit_id}>
+                      {vehicle.number}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            {/* Time period selection */}
+            <div className="time">
+              <span>Period</span>
+              <div className="time-margin">
+                <div className="column">
+                  <label htmlFor="startDate">From</label>
+                  <input
+                    type="date"
+                    id="startDate"
+                    name="startDate"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                    pattern="\d{4}-\d{2}-\d{2}"
+                    required
+                  />
+                </div>
+                <div className="column">
+                  <label htmlFor="endDate">To</label>
+                  <input
+                    type="date"
+                    id="endDate"
+                    name="endDate"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                    pattern="\d{4}-\d{2}-\d{2}"
+                    required
+                  />
                 </div>
               </div>
-
-              <div className="time">
-                <span>Period</span>
-                <div className="time-margin">
-                  <div className="column">
-                    <label htmlFor="startDate">From</label>
-                    <input
-                        type="date"
-                        id="startDate"
-                        name="startDate"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                        pattern="\d{4}-\d{2}-\d{2}"
-                        required
-                    />
-                  </div>
-                  <div className="column">
-                    <label htmlFor="endDate">To</label>
-                    <input
-                        type="date"
-                        id="endDate"
-                        name="endDate"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                        pattern="\d{4}-\d{2}-\d{2}"
-                        required
-                    />
-                  </div>
-                </div>
-              </div>
-            </form>
-
-            <div className="map-container">
-              { routeCoordinates && routeCoordinates.length > 0 ? (
-                  <LoadScript
-                      googleMapsApiKey={process.env.REACT_APP_MAP_KEY}
-                      onError={() => alert('Error loading Google Maps. Please check the console for details.')}
-                  >
-                    <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
-                      { routeCoordinates.map((route, index) => (
-                          <Fragment key={index}>
-                            <Marker
-                                position={{ lat: route.start.lat, lng: route.start.lng }}
-                            />
-                            <Marker
-                                position={{ lat: route.start.lat, lng: route.start.lng }}
-                            />
-                            <Polyline
-                                path={route.decoded_route.points.map((point) => ({
-                                  lat: point.lat,
-                                  lng: point.lng
-                                }))}
-                                options={{ strokeColor: '#FF0000', strokeWeight: 4 }}
-                            />
-                          </Fragment>
-                      ))}
-                    </GoogleMap>
-                  </LoadScript>
-                ) : (
-                  <div>Test</div>
-                )
-              }
             </div>
-            <div>
-        <p>Total Distance: {totalDistance.toFixed(2)} km</p>
-        <p>Total Time: {Math.floor(totalTime)} hours {Math.floor((totalTime % 1) * 60)} minutes</p>
-      </div>
-            <div className="container-button">
-              <button type="button" onClick={generateRoute}>
-                GENERATE
-              </button>
+          </form>
+
+          {/* Map container */}
+          <div className="map-container">
+            {routeCoordinates && routeCoordinates.length > 0 ? (
+              <LoadScript
+                googleMapsApiKey={process.env.REACT_APP_MAP_KEY}
+                onError={() => alert('Error loading Google Maps. Please check the console for details.')}
+              >
+                <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
+                  {routeCoordinates.map((route, index) => (
+                    <Fragment key={index}>
+                      <Marker position={{ lat: route.start.lat, lng: route.start.lng }} />
+                      <Marker position={{ lat: route.start.lat, lng: route.start.lng }} />
+                      <Polyline
+                        path={route.decoded_route.points.map((point) => ({
+                          lat: point.lat,
+                          lng: point.lng
+                        }))}
+                        options={{ strokeColor: '#FF0000', strokeWeight: 4 }}
+                      />
+                    </Fragment>
+                  ))}
+                </GoogleMap>
+              </LoadScript>
+            ) : (
+              <div></div>
+            )}
+          </div>
+
+        {/* Route details */}
+        {routeCoordinates && routeCoordinates.length > 0 && (
+          <div className="containers-details">
+            <div className="distance">
+              <p>{totalDistance.toFixed(0)}</p>
+              <p className="text">Km Driven</p>
             </div>
+            <div className="driving-time">
+              <p>{totalTime.hours}h {totalTime.minutes}m</p>
+              <p className="text">Driving Time</p>
+            </div>
+          </div>
+        )}
+
+          {/* Generate route button */}
+          <div className="container-button">
+            <button type="button" onClick={generateRoute}>
+              GENERATE
+            </button>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
